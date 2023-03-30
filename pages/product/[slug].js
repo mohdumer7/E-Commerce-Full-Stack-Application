846a19f84f686d4120e99241d951bf600061e3d1
mohdumer7 <umermohammed95@gmail.com>
2023-03-31 01:20:30 +0530

    Added Dynamic Routing

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,5 +1,6 @@
+import { Product } from "@/components";
 import { client, urlFor } from "@/lib/client";
-import React from "react";
+import React, { useState } from "react";
 import {
   AiOutlineMinus,
   AiOutlinePlus,
@@ -9,20 +10,31 @@ import {
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price, desc } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} />
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
           </div>
-          {/* <div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item, i) => (
-              <img key={i} src={urlFor(item)} className="" onMouseEnter="" />
+              <img
+                key={i}
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
             ))}
-          </div> */}
+          </div>
         </div>
-        <div className="product-details-desc">
+        <div className="product-detail-desc">
           <h1>{name}</h1>
           <div className="reviews">
             <div>
@@ -30,14 +42,13 @@ const ProductDetails = ({ product, products }) => {
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
+              <AiOutlineStar />
             </div>
             <p>(20)</p>
           </div>
           <h4>Details:</h4>
           <p>{details}</p>
-          <p className="price" style={{ fontWeight: "600" }}>
-            ${price}
-          </p>
+          <p className="price">${price}</p>
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
@@ -59,6 +70,16 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="buy-now" onClick="">
               Buy Now
             </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You May Also Like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
           </div>
         </div>
       </div>
