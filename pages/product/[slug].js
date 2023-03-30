4e26038c5c6cd2657658e1e42631f298fac8edb4
mohdumer7 <umermohammed95@gmail.com>
2023-03-30 22:54:11 +0530

    Added Dynamic Routing

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,19 +1,91 @@
 import { client, urlFor } from "@/lib/client";
 import React from "react";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineStar,
+  AiFillStar,
+} from "react-icons/ai";
 
 const ProductDetails = ({ product, products }) => {
-  const { imageConfigDefault, name, details, price } = product;
+  const { image, name, details, price, desc } = product;
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src="" />
+            <img src={urlFor(image && image[0])} />
+          </div>
+          {/* <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img key={i} src={urlFor(item)} className="" onMouseEnter="" />
+            ))}
+          </div> */}
+        </div>
+        <div className="product-details-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price" style={{ fontWeight: "600" }}>
+            ${price}
+          </p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+    slug {
+      current
+    }
+  }
+  `;
+
+  const products = await client.fetch(query);
+
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: "blocking",
+  };
 };
 
 export const getStaticProps = async ({ params: { slug } }) => {
