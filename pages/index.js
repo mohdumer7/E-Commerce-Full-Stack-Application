cd230b49e212b8a7ec38faa089e24e1663e270fb
mohdumer7 <umermohammed95@gmail.com>
2023-03-30 17:53:10 +0530

    Added Client and Assests

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,36 @@
 import React from "react";
 import { Product, HeroBanner, FooterBanner } from "../components";
+import { client } from "../lib/client";
 
-const Home = () => {
+const Home = ({ products, banner }) => {
   return (
     <>
-      HeroBanner
+      <HeroBanner heroBanner={banner.length && banner[0]} />
+      {console.log(banner)}
       <div className="products-heading">
         <div>
           <h2>Best Selling products</h2>
           <p>Speakers of Many Variations</p>
           <div className="products-container">
-            {["Product1", "Product2", "Product3"].map((product) => product)}
+            {products?.map((product) => product.name)}
           </div>
         </div>
       </div>
-      Footer
+      <FooterBanner />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  const productQuery = "*[_type == 'product']";
+  const products = await client.fetch(productQuery);
+
+  const bannerQuery = "*[_type == 'banner']";
+  const banner = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, banner },
+  };
 };
 
 export default Home;
